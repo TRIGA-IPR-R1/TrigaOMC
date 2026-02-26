c94fd25c3a77e9b34e036bb6326d88df5f856cea
Thalles Campagnani <thallescampagnani@gmail.com>
2026-02-26 19:40:48 -0300

    Criando pasta para plots

--- openMC/simulando.py
+++ openMC/simulando.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#!/bin/python
 
 #######################################################################
 ####                                                               ####
@@ -26,9 +27,14 @@ libTrigaIprR1.mkdir(voltar=False, nome="resultados", data=True, cpinputs=True)
 
 # Criando reator
 triga = libTrigaIprR1.TrigaIprR1()
+#triga.geometria(tipo_geometria="hexagonal")
 
-
-libTrigaIprR1.mkdir(voltar=False, nome="keff", data=False, cpinputs=False)
+# Plotanto geometria
+libTrigaIprR1.mkdir(voltar=False, nome="plots", data=False)
+triga.plot2D_secao_transversal()
+triga.plot2D_secao_transversal(basis="xz")
+triga.plot2D_secao_transversal(basis="yz")
 
 # Simulação de autovalor (keff, fulga, etc)
+libTrigaIprR1.mkdir(voltar=True, nome="keff", data=False)
 triga.simulacao_autovalor()
